13a04aedc5a9f0ac60743e14aeda76a32a6e5aaa
airik31 <l.erik321@gmail.com>
2021-04-28 22:46:30 +0200

    Add prompt for user that then generates a greeting in the HTML

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function test () {
-if (i > 3) {
-    alert("hello world")
+function getUserName () {
+let username = prompt("Hello dear visitor, what is your name?");
+document.getElementById("username").innerHTML = `<h3>Welcome ${username}! I am happy to see you here browsing my portfolio website, have a nice day!<h3>`
 }
-}
+
+getUserName();
