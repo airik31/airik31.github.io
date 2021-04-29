bda5eb830773bb3ff557a43f0bab4aec7367963f
airik31 <l.erik321@gmail.com>
2021-04-29 18:26:09 +0200

    Delete script, added more CSS styling to the elements, background color and positioning

--- script.js
+++
@@ -1,6 +0,0 @@
-function getUserName () {
-let username = prompt("Hello dear visitor, what is your name?");
-document.getElementById("username").innerHTML = `<h3>Welcome ${username}! I am happy to see you here browsing my portfolio website, have a nice day!<h3>`
-}
-
-getUserName();
